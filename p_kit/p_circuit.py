dd3278e351039f3d571d99e5e391906dd1f597bd
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-07-20 21:15:10 +0200

    i and m implementation as per Pervaiz et al.

--- p_kit/p_circuit.py
+++ p_kit/p_circuit.py
@@ -1,6 +1,16 @@
 """Module for pipelines."""
 import numpy as np
+from random import random
+from math import tanh
 
+def rand(min, max):
+    return random() * (max - min) - min
+
+def S(X):
+    return tanh(X)
+
+def sign(X):
+    return X / abs(X)
 
 class PCircuit():
 
@@ -28,3 +38,10 @@ class PCircuit():
         self.n_pbits = n_pbits
         self.h = np.zeros((n_pbits,1))
         self.J = np.zeros((n_pbits, n_pbits))
+        self.i0 = 0
+    
+    def i(self, pbit, t):
+        return self.i0 * (self.h[pbit] + sum(self.J[pbit, j] * self.m(pbit, t) for j in range(self.n_pbits) if not j == pbit))
+
+    def m(self, pbit, t):
+        return sign(rand(-1, 1) + S(self.i(pbit, t)))
