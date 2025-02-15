21390a9df3f07ad48124af1ab67b4f9ccdf37821
Gregoire Cattan <gregoire.cattan@ibm.com>
2025-02-15 22:42:45 +0100

    improve annealing method to return more samples

--- examples/tsp.py
+++ examples/tsp.py
@@ -5,20 +5,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from p_kit.library.tsp import TSP
-from p_kit.solver.annealing import execute, linear
+from p_kit.solver.annealing import constant, execute, linear
 from p_kit.solver.csd_solver import CaSuDaSolver
 from p_kit.visualization.tsp_graph import visualize_tsp_route
 from p_kit.visualization.utils import tsp_hist
 
 # Define city graph (distance matrix)
-city_graph = np.array([[0, 510, 480],
-                      [510, 0, 240],
-                      [480, 240, 0]])
+# city_graph = np.array([[0, 510, 480],
+#                       [510, 0, 240],
+#                       [480, 240, 0]])
 
-# city_graph = np.array([[0, 510, 480, 490],
-#               [510, 0, 240, 370],
-#               [480, 240, 0, 220],
-#               [490, 370, 220, 0]])
+city_graph = np.array([[0, 510, 480, 490],
+              [510, 0, 240, 370],
+              [480, 240, 0, 220],
+              [490, 370, 220, 0]])
+# city_graph = np.array([[  0.,1.,1.,1.,-10.,-10.,1.,-15.,-15.],
+#  [  1.,0.,1.,-10.,1.,-10.,-15.,1.,-15.],
+#  [  1.,1.,0.,-10.,-10.,1.,-15.,-15.,1.],
+#  [  1.,-10.,-10.,0.,1.,1.,-20.,-20.,-20.],
+#  [-10.,1.,-10.,1.,0.,1.,-20.,-20.,-20.],
+#  [-10.,-10.,1.,1.,1.,0.,-20.,-20.,-20.],
+#  [  1.,-15.,-15.,-20.,-20.,-20.,0.,1.,1.],
+#  [-15.,1.,-15.,-20.,-20.,-20.,1.,0.,1.],
+#  [-15.,-15.,1.,-20.,-20.,-20.,1.,1.,0.]])
 
 # Convert to negative weights for optimization
 city_graph *= -1
@@ -27,11 +36,11 @@ city_graph *= -1
 circuit = TSP(city_graph=city_graph, tsp_modifier=0.33)
 
 # Simulated Annealing Schedule
-solver = CaSuDaSolver(Nt=int(1e5), dt=1 / (2 * len(city_graph)), i0=0, expected_mean=0, seed=None)
+solver = CaSuDaSolver(Nt=int(1e6), dt=1 / (2 * len(city_graph)), i0=0.005, expected_mean=0, seed=None)
 
 # Increase n_shots to have more reliable results
 n_shots = 100
-samples = execute(solver, circuit, linear, n_shots, n_jobs=-1)
+samples = execute(solver, circuit, constant, n_shots, n_last_samples=50, n_jobs=-1)
 
 # Visualize Best path
 hist = tsp_hist(samples, city_graph)
--- p_kit/solver/annealing.py
+++ p_kit/solver/annealing.py
@@ -8,11 +8,11 @@ def constant(solver, _run):
 def linear(solver, run):
     return (0 - solver.i0) / solver.Nt * (run - 1) + solver.i0
 
-def execute(solver, circuit, annealing, n_shots, n_jobs=-1):
+def execute(solver, circuit, annealing, n_shots, n_last_samples=50, n_jobs=-1):
     def job():
         _, time_points, _ = solver.copy().solve(circuit.copy(), annealing_func=annealing)
-        return time_points[-1, :]
+        return time_points[-n_last_samples-1:-1, :]
 
     samples = Parallel(n_jobs=n_jobs)(delayed(job)() for _ in range(n_shots))
 
-    return np.array(samples)
+    return np.array(samples).reshape((n_shots * n_last_samples, circuit.n_pbits))
