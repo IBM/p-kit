5d83b4d09cc6fcab6b7b76bd16eb605aeaeb12fa
Gregoire Cattan <gregoire.cattan@ibm.com>
2025-02-25 11:31:45 +0100

    add back heatmap

--- p_kit/visualization/__init__.py
+++ p_kit/visualization/__init__.py
@@ -3,8 +3,10 @@ from .histplot import histplot, energyplot
 from .vin_vout import vin_vout
 from .plot3d import plot3d
 from .tsp_graph import visualize_tsp_route
+from .heatmap import heatmap
 
 __all__ = [
+    "heatmap",
     "m_to_string",
     "tsp_hist",
     "histplot",
